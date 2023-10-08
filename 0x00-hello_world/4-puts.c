f07107b65c99043925177e1adfd309051b6a6aab
ziadhunter <farouk.ziad.2002@gmail.com>
2023-10-07 18:26:08 -0700

    done

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ *main - entry point
+ *
+ * Description : A C program that print with put fonction
+ *
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * description: a c program that prints with printf function.
+ *
+ * Return: always 0 (successs
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
