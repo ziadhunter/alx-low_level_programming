20fcb6b7aff3519ba8b825edd5340cb3f42d57e0
ziadhunter <farouk.ziad.2002@gmail.com>
2023-10-09 14:18:25 -0700

    done

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point for the program.
+ *
+ * Description: print the value of n status:
+ * grates than ,is zero and is not less than 6.
+ *
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	int n = 0;
+
+	while  (n <= 9)
+	{
+		printf("%d", n);
+		n++;
+	}
+	printf("\n");
+
+	return (0);
+}
+
