edd283eab1f86c5e2e68bf95182406ea33ec9726
ziadhunter <farouk.ziad.2002@gmail.com>
2023-10-09 13:03:14 -0700

    done

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/*
+ *main - Entry point for the program.
+ *
+ *description; this is a program that print the alphabet,a to z
+ *
+ *Return: always (0) successe
+ */
+
+int main(void)
+{
+	int a = 97;
+	int i;
+
+	for (i = a; i <= z; i++)
+	{
+		putchar("%d", i);
+	}
+	putchar("\n");
+
+	return (0);
+}
