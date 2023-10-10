43a907d320eb4b6cc0d44888c1af66a2966ec2d0
ziadhunter <farouk.ziad.2002@gmail.com>
2023-10-10 16:06:25 -0700

    done

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include <main.h>
+
+/**
+ * main - Entry value
+ *
+ * description: print the alphabet using _putchar fonction
+ *
+ * return: always (0) success
+ */
+
+int main(void)
+{
+	char "e";
+
+	for(e = "a"; e <= "z"; e++)
+		_putchar(e);
+	_putchar('\n');
+
+	return (0);
+}
